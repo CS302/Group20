3a00802d68bc659b13031870d95b38826a6cc095
CS302 <luckycloud@yandex.ru>
2017-04-11 21:01:13 +0400

    Lesson11
    Lesson11

--- WorkersEditor/MainWindow.xaml.cs
+++ WorkersEditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,11 @@ namespace WorkersEditor
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<Worker> Workers { get; set; }
+        public ObservableCollection<Worker> Workers { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            Workers = new List<Worker>();
+            Workers = new ObservableCollection<Worker>();
             Workers.Add(new Driver("John", 28, 123456, 256, "BMW"));
             Workers.Add(new Driver("Ivan", 38, 456123, 128, "UAZ"));
             Workers.Add(new Manager("Hulk", 58, 789456, 15));
@@ -41,6 +42,26 @@ namespace WorkersEditor
             if (worker != null)
             {
                 detailsPanel.DataContext = worker;
+                if (worker is Driver)
+                {
+                    manInfo.Visibility = System.Windows.Visibility.Collapsed;
+                    drInfo.Visibility = System.Windows.Visibility.Visible;
+                }
+                if (worker is Manager)
+                {
+                    manInfo.Visibility = System.Windows.Visibility.Visible;
+                    drInfo.Visibility = System.Windows.Visibility.Collapsed;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Worker delWorker = workersList.SelectedItem as Worker;
+            if (delWorker != null)
+            {
+                Workers.Remove(delWorker);
+                workersList.SelectedIndex = 0;
             }
         }
     }
